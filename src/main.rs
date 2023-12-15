9628129106b884128818b31b0eea57b2ff5be106
Mahmoud Almontasser <almontasser@outlook.com>
2023-12-15 22:27:12 +0200

    implement the lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let source =
+        "fn main() { print(\"Hello, world!\"); let x = 1; let y = 2; print(x + y); return; }";
+    let mut lexer = lexer::Lexer::new(source.to_string());
+    let tokens = lexer.scan_tokens().unwrap();
+    for token in tokens {
+        println!("{:?}", token);
+    }
 }
