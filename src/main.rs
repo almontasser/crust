dd0f4e52e07d2edd24941a28b6be62f8b913267a
Mahmoud Almontasser <almontasser@outlook.com>
2023-12-17 10:22:32 +0200

    Removed interpreter function in main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,6 @@ mod codegen;
 mod lexer;
 mod parser;
 
-fn interpre(node: Node) -> i64 {
-    match node {
-        Node::LiteralExpr { value } => match value {
-            Literal::Integer(i) => i as i64,
-        },
-        Node::BinaryExpr {
-            left,
-            operator,
-            right,
-        } => {
-            let left = interpre(*left);
-            let right = interpre(*right);
-
-            match operator.token_type {
-                lexer::TokenType::Add => left + right,
-                lexer::TokenType::Sub => left - right,
-                lexer::TokenType::Mul => left * right,
-                lexer::TokenType::Div => left / right,
-                _ => panic!("Unexpected token {:?}", operator),
-            }
-        }
-        Node::UnaryExpr { operator, right } => {
-            let right = interpre(*right);
-
-            match operator.token_type {
-                lexer::TokenType::Sub => -right,
-                _ => panic!("Unexpected token {:?}", operator),
-            }
-        }
-    }
-}
-
 fn main() {
     let source = String::from("2 + 3 * 5 - 8 / 3;");
     let mut lexer = lexer::Lexer::new(source);
