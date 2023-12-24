5ff4a6682542b84445cd81a67e76cf0d1779fd24
Mahmoud Almontasser <almontasser@outlook.com>
2023-12-24 22:56:55 +0200

    Remove unused commented code

--- src/main.rs
+++ src/main.rs
@@ -156,13 +156,6 @@ fn main() {
     let source = std::fs::read_to_string(&args[1]).expect("Failed to read file");
     // let source = std::fs::read_to_string("tests/test17").expect("Failed to read file");
 
-    // let source = String::from(
-    //     "fn main() {
-    //         printint(1);
-    //       }
-    //     ",
-    // );
-
     let mut lexer = lexer::Lexer::new(source);
     let tokens = lexer.scan_tokens();
     let mut parser = parser::Parser::new(tokens.clone());
